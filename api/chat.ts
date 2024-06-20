42cf2cde68a810cb37ffaf44d32855035140f97a
Hao Nan Huang <hnxhu2065@gmail.com>
2024-06-20 14:48:58 -0400

    adding api dir

---
+++ api/chat.ts
@@ -0,0 +1,44 @@
+import { WebClient } from "@slack/web-api"
+import { generatePrompt, getChatGPTResponse } from "./openai"
+import { stringify } from "querystring"
+
+const slack = new WebClient(process.env.SLack_BOT_TOKEN)
+
+type Event = {
+    channel: string
+    timestamp: string
+    thread_timestamp?: string
+}
+
+export async function sendChatGPTresponse(event: Event) {
+    const {channel, timestamp, thread_timestamp} = event
+
+    try {
+        const thread = await slack.conversations.replies({
+            channel,
+            ts: thread_timestamp ?? timestamp,
+            inclusive: true
+        })
+
+        const prompts = await generatePrompt(thread)
+        const responseFromChatGPT = await getChatGPTResponse(prompts)
+
+        await slack.chat.postMessage({
+            channel,
+            thread_ts: timestamp,
+            text: `${responseFromChatGPT.choices[0].message.content}`,
+        })
+    } catch(error) {
+        if(error instanceof Error){
+            await slack.chat.postMessage({
+                channel,
+                thread_ts: timestamp,
+                text: `<@${process.env.SLACK_ADMIN_NUMBER_ID}> Error: ${error.message}`,
+
+            })
+
+        }
+
+    }
+
+}
