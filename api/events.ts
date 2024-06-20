cd447cad2f1058d49dbf6574ab594dfb0872a998
Hao Nan Huang <haonanhuang@Haos-MacBook-Pro.local>
2024-06-20 14:11:22 -0400

    Initial commit

---
+++ api/events.ts
@@ -0,0 +1,41 @@
+import { sendChatGPTresponse } from "./chat"
+import crypto from "crypto"
+
+export const config = {max_duration: 30,}
+
+async function isValidSlackRequest(request: Request, body: any) {
+    const signing_secret = process.env.SLACK_SIGNING_SECRET!
+    const timestamp = request.headers.get("X-Slack-Request-Timestamp")
+    const slack_signature = request.headers.get("X-Slack-signature")
+    const base = `v0:${timestamp}:${JSON.stringify(body)}`
+    const hmac = crypto
+        .createHmac("sha256", signing_secret)
+        .update(base)
+        .digest("hex")
+
+    const computed_signature = `v0=${hmac}`
+    return computed_signature === slack_signature
+
+}
+
+export async function POST(request: Request) {
+    const raw_body = await request.text()
+    const body = JSON.parse(raw_body)
+    const request_type = body.request_type
+
+    if(request_type === "url_verification") {
+        return new Response(body.challenge, {status: 200})
+    }
+
+    if(await isValidSlackRequest(request, body)) {
+        if(request_type === "event_callback") {
+            const event_type = body.event.type
+            if(event_type === "app_mention"){
+                await sendChatGPTresponse(body.event)
+                return new Response("Done!", {status: 200})
+            }
+        }
+    }
+
+    return new Response('Unauthorized', { status: 401 })
+}
---
+++ api/openai.ts
@@ -0,0 +1,34 @@
+import type { ChatCompletionMessageParam } from 'openai/src/resources/index.js'
+import type { ConversationsRepliesResponse } from "@slack/web-api"
+import OpenAI from 'openai'
+
+const openai = new OpenAI()
+
+export async function getChatGPTResponse(messages: ChatCompletionMessageParam[]){
+    return await openai.chat.completions.create({
+        model: "gpt-3.5",
+        messages
+    })
+}
+
+export async function generatePrompt({messages}: ConversationsRepliesResponse) {
+    if(!messages) throw new Error("No message found")
+    const botID = messages[0].reply_users?.[0]
+    const result = messages
+
+        .map((message: any) => {const isBot = !!message.bot_id && !message.client_msg_id
+                                const isNotMentioned = !isBot && !message.text.startsWith(`<@`)
+                                if (isNotMentioned) {
+                                    return null
+                                }
+                                return {
+                                    role: isBot ? "assistant" : "user",
+                                    content: isBot
+                                        ? message.text
+                                        : message.text.replace(`<@${botID}`, "")
+                                }
+        })
+        .filter(Boolean)
+    return result as ChatCompletionMessageParam[]
+}
+
