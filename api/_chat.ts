fd75e323fb830b38e90216d9f3dcacd19f313bc2
HaoNan Huang <108586883+ProgRiseK@users.noreply.github.com>
2024-07-07 16:56:12 -0400

    correct minor error in _chat.ts

--- api/_chat.ts
+++ api/_chat.ts
@@ -2,7 +2,7 @@ import { WebClient } from "@slack/web-api"
 import { generatePrompt, getChatGPTResponse } from "./_openai"
 import { stringify } from "querystring"
 
-const slack = new WebClient(process.env.SLack_BOT_TOKEN)
+const slack = new WebClient(process.env.SLACK_BOT_TOKEN)
 
 type Event = {
     channel: string
