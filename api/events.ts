b13da954150204fead4a0b6792d13a9b6ca23f63
HaoNan Huang <108586883+ProgRiseK@users.noreply.github.com>
2024-07-07 16:49:21 -0400

    correct minor error in "events.ts"

--- api/events.ts
+++ api/events.ts
@@ -5,15 +5,15 @@ export const config = {max_duration: 30}
 
 async function isValidSlackRequest(request: Request, body: any) {
     const signing_secret = process.env.SLACK_SIGNING_SECRET!
-    const timestamp = request.headers.get("X-Slack-Request-Timestamp")
-    const slack_signature = request.headers.get("X-Slack-signature")
+    const timestamp = request.headers.get("X-Slack-Request-Timestamp")!
+    const slack_signature = request.headers.get("X-Slack-Signature")!
     const base = `v0:${timestamp}:${JSON.stringify(body)}`
     const hmac = crypto
         .createHmac("sha256", signing_secret)
         .update(base)
         .digest("hex")
-
     const computed_signature = `v0=${hmac}`
+    
     return computed_signature === slack_signature
 
 }
@@ -21,7 +21,7 @@ async function isValidSlackRequest(request: Request, body: any) {
 export async function POST(request: Request) {
     const raw_body = await request.text()
     const body = JSON.parse(raw_body)
-    const request_type = body.request_type
+    const request_type = body.type
 
     if(request_type === "url_verification") {
         return new Response(body.challenge, {status: 200})
